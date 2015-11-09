66bcf390b89bf072a54923bf501e138c083e2f3c
robinkun <robinkun96@gmail.com>
2015-11-09 14:30:26 +0900

    make readMap & map struct

--- map.c
+++ map.c
@@ -1,1 +1,41 @@
 #include "map.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void initMap(map* h, int width, int height) {
+  int i;
+  h->field = (int**)malloc(sizeof(int*)*height);
+  h->width = width;
+  h->height = height;
+
+  for(i = 0; i < width; i++) {
+    h->field[i] = (int*)malloc(sizeof(int)*width);
+  }
+}
+
+void readMap(map* p, char* dir) {
+  FILE* fp;
+  char ch = '0';
+  int x, y;
+
+  fp = fopen(dir, "rb");
+  if(fp == NULL) {
+    printf("Can't open map file.");
+    return;
+  }
+
+  for(y = 0; y < p->height; y++) {
+    for(x = 0; x < p->width; x++) {
+      ch = getc(fp);
+      printf("%3X", ch);
+      if(ch == EOF) {
+        printf("MapFile format error.");
+        return;
+      }
+      p->field[y][x] = ch;
+    }
+    puts("");
+  }
+
+  fclose(fp);
+}
--- map.h
+++ map.h
@@ -1,6 +1,12 @@
 #ifndef __MAP_H__
 #define __MAP_H__
 
+typedef struct map{
+  int** field;
+  int width, height;
+} map;
 
+void readMap(map* p, char* dir);
+void initMap(map* h, int width, int height);
 
 #endif
