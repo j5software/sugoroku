898ebbdfda6d44ea321e9945c4c6d28be710b3c9
1koba <1koba360@gmail.com>
2016-01-05 16:16:16 +0900

    追記しました

--- dispmap.h
+++ dispmap.h
@@ -1,8 +1,12 @@
 #ifndef __DISPMAP_H__
 #define __DISPMAP_H__
 
+#include "sugoroku.h"
+#include "player.h"
+#include "position.h"
 #include "map.h"
 
-void disp_map(Map* p);
+int dispmap(Map* s, int x, int y, Player* p, int width, int height);
+//int disp_map(Map* s, int x, int y, Position p, int width, int height);
 
 #endif
