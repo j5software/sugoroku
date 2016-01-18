546774a6efa60d93e2f4f2fed96f6e2e5711eb05
Hajime Kosaku <kosakku0105@gmail.com>
2016-01-18 14:21:23 +0900

    アイテムのアイデアを10個追加．

--- item.h
+++ item.h
@@ -8,5 +8,15 @@ typedef struct Item
 } Item;
 
 void initItem(Item *i);
+void useItem1();
+void useItem2();
+void useItem3();
+void useItem4();
+void useItem5();
+void useItem6();
+void useItem7();
+void useItem8();
+void useItem9();
+void useItem10();
 
 #endif
