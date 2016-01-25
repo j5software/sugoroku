b7fe70f177ae66cb83cb7b37b0b2b87c065f9b51
Hajime Kosaku <kosakku0105@gmail.com>
2016-01-25 14:32:02 +0900

    make Item4&5.

--- item.h
+++ item.h
@@ -2,6 +2,7 @@
 #define __ITEM_H__
 
 #include "player.h"
+#include "position.h"
 
 typedef struct Item
 {
@@ -13,12 +14,13 @@ void initItem(Item *i);
 void useItem1(Player *p, int player_num);
 void useItem2(Player *p, int get_player, int player_maxnum);
 void useItem3();
-void useItem4();
-void useItem5();
+void useItem4(Player *p, int player_num);
+void useItem5(Player *p, int player_maxnum);
 void useItem6();
 void useItem7();
 void useItem8();
 void useItem9();
 void useItem10();
+void changePosition(Player *p, int num1, int num2);
 
 #endif
