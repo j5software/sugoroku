4166292f4c3ad47c3c0398ed1d5bd7d8d154fe9f
robinkun <robinkun96@gmail.com>
2016-02-16 17:28:01 +0900

    便利な処理関数を追加

--- bag.c
+++ bag.c
@@ -1,9 +1,52 @@
 #include "bag.h"
+#include <stdlib.h>
 
-void initBag(Bag* b) {
+void initBag(Bag *b, int bag_size) {
   int i;
-  for(i = 0; i < BAG_SIZE; i ++) {
-    b->items[i][0] = -1; //-1で何も持ってない　アイテム番号を格納する
-    b->items[i][1] = -1; //-1で何も持ってない　アイテム個数を格納する
+  b->size = bag_size;
+  b->items = (BagItem *)malloc(sizeof(BagItem)*bag_size);
+  for(i = 0; i < bag_size; i ++) {
+    b->items[i].item_id = 0; // アイテム番号を格納する
+    b->items[i].num = 0; // アイテム個数を格納する
   }
+}
+
+void deleteBag(Bag *b) {
+  if(b->items)
+    free(b->items);
+}
+
+int addBagItem(Bag *b, int item_id, int num) {
+  int i;
+  for(i = 0; i < b->size; i++) {
+    if(b->items[i].item_id == item_id) {
+      b->items[i].item_id = item_id;
+      b->items[i].num++;
+      return 1;
+    }
+  }
+  for(i = 0; i < b->size; i++) {
+    if(b->items[i].num == 0) {
+      b->items[i].item_id = item_id;
+      b->items[i].num++;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int popBagItem(Bag *b, int bag_num) {
+  int i;
+  if(b->items[bag_num].num <= 0) {
+    return -1;
+  }
+
+  b->items[bag_num].num--;
+  // 詰める
+  if(b->items[bag_num].num <= 0) {
+    for(i = bag_num; i < b->size-1; i++) {
+      b->items[i] = b->items[i+1];
+    }
+  }
+  return b->items[bag_num].item_id;
 }
--- bag.h
+++ bag.h
@@ -2,12 +2,21 @@
 #define __BAG_H__
 #define BAG_SIZE 20
 
+typedef struct BagItem {
+  int item_id;
+  int num; //個数
+} BagItem;
+
 typedef struct Bag
 {
   // 1アイテム番号,2は個数
-  int items[BAG_SIZE][2];
+  BagItem *items;
+  int size;
 } Bag;
 
-void initBag(Bag* b);
+void initBag(Bag *b, int bag_size);
+void deleteBag(Bag *b);
+int addBagItem(Bag *b, int item_id, int num);
+int popBagItem(Bag *b, int bag_num);
 
 #endif
